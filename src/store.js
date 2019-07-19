dc11c388535cf892b2c206b142d637d2d15bfc95
Colleen <colleenhiggins87@gmail.com>
2019-07-19 09:30:23 -0700

    refactored server into routers

---
+++ src/store.js
@@ -0,0 +1,18 @@
+/* eslint-disable strict */
+const cards = [
+  {
+    id: 1,
+    title: 'Task One',
+    content: 'This is card one'
+  }
+];
+
+const lists = [
+  {
+    id: 1,
+    header: 'List One',
+    cardIds: [1]
+  }
+];
+
+module.exports = { cards, lists}
